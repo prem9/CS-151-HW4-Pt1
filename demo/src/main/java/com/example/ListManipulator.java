270bb4a8dbdb259c074861c88b4623be3d436dc8
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-10-19 20:40:28 -0700

    eh

--- demo/src/main/java/com/example/ListManipulator.java
+++ demo/src/main/java/com/example/ListManipulator.java
@@ -1,4 +1,7 @@
 package com.example;
+    
+import java.util.*;
+
 
 /**
  * Hello world!
@@ -6,8 +9,27 @@ package com.example;
  */
 public class ListManipulator 
 {
+    public ArrayList<Integer> sort(ArrayList<Integer> myLst, Boolean ascending)
+    {
+        if (ascending)
+        {
+            Collections.sort(myLst);
+            return myLst;
+        }
+        else
+        {
+            Collections.sort(myLst, Collections.reverseOrder());
+            return myLst;
+        }
+    }
+    public ArrayList<Integer> swapLargestSmallest(ArrayList<Integer> myLst)
+    {
+
+        return myLst;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    
 }
