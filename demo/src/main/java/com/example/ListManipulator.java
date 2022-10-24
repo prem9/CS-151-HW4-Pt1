7c04f1a39ab99214c656995962ae4bafaa3a3752
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-10-24 13:32:07 -0700

    nearly completed

--- demo/src/main/java/com/example/ListManipulator.java
+++ demo/src/main/java/com/example/ListManipulator.java
@@ -24,8 +24,39 @@ public class ListManipulator
     }
     public ArrayList<Integer> swapLargestSmallest(ArrayList<Integer> myLst)
     {
-
+        Integer largest;
+        largest = myLst.get(0);
+        int i = 0;
+        int largestIndex = 0;
+        for(Integer num: myLst)
+        {
+            if (largest < num)
+            {
+                largest = num;
+                largestIndex = i;
+            }
+            i++;
+        }
+        i = 0;
+        Integer smallest = myLst.get(0);
+        int smallestIndex = 0;
+        for(Integer num: myLst)
+        {
+            if (smallest > num)
+            {
+                smallest = num;
+                smallestIndex = i;
+            }
+            i++;
+            
+        }
+        myLst.set(largestIndex, smallest);
+        myLst.set(smallestIndex, largest);
         return myLst;
+    }
+    public void table(ArrayList<Integer> myLst)
+    {
+
     }
     public static void main( String[] args )
     {
